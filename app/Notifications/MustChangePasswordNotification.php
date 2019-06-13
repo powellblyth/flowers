98e52201b574849e14b5f81f9850ac8adba9cff1
Toby Powell-Blyth <toby@thegiltclub.com>
2019-06-13 21:32:34 +0100

    Reset message for resetting passwords

--- app/Notifications/MustChangePasswordNotification.php
+++ app/Notifications/MustChangePasswordNotification.php
@@ -54,14 +54,16 @@ class MustChangePasswordNotification extends Notification {
 
         return (new MailMessage)
             ->greeting($greeting)
-            ->subject(Lang::getFromJson('Welcome to the PHS Entries and Membership system'))
+            ->subject(Lang::getFromJson('UPDATE !!! Welcome to the PHS Entries and Membership system'))
+            ->line(Lang::getFromJson('With apologies for problems occurring with our past email!  '))
+            ->line(Lang::getFromJson('All new systems have teething troubles, and this is no exception. The links you received earlier only lasted 60 minutes, which was not our intention. As a result, we are slightly altering the instructions. In order to set your password..'))
+            ->line(Lang::getFromJson('To reset your password, simply pretend that you have forgotten your password. Please go to this page'))
+            ->action(Lang::getFromJson('I am pretending to have \'Forgotten\' my password!'), url(config('app.url') . route('password.request', false)))
+            ->line(Lang::getFromJson('(original message follows)'))
             ->line(Lang::getFromJson('The Petersham Horticultural Society is delighted to have a brand new online entry system. '))
             ->line(Lang::getFromJson( 'We would love you to be the first to try it. On this system you can create your entries, and as time goes on, we hope to implement a system to allow you to pay online, and purchase membership. It\'s the Future!'))
             ->line(Lang::getFromJson('We hope you are still happy for us to contact you via Email - if this has changed please let us know'))
             ->line(Lang::getFromJson('Where multiple people (i.e. other members of your family!) appear to be at the same address, we have endeavoured to group you all together to be managed with a single username and password. Do let me know if I got it wrong.'))
-            ->line(Lang::getFromJson('All you need to do is follow the link below, set yourself a password.'))
-            ->action(Lang::getFromJson('Set me a password!'), url(config('app.url') . route('password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset(), 'initialpassword'=>1], false)))
-            ->line(Lang::getFromJson('Please Note: This password reset link will expire once advanced entries stop on the Wednesday before the show'))
             ->line(Lang::getFromJson('If you think you received this in error, please feel free to let us know by emailing enquiries@petershamhorticulturalsociety.org.uk'));
     }
 
