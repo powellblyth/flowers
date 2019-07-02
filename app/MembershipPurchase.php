6bc3ebc06efb46cf57a48e13a7445960f3b0a5b2
Toby Powell-Blyth <toby@thegiltclub.com>
2019-07-02 22:10:32 +0100

    Add ORM columns to membership purchases

--- app/MembershipPurchase.php
+++ app/MembershipPurchase.php
@@ -3,23 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class MembershipPurchase extends Model
-{
+class MembershipPurchase extends Model {
     public function getNumber(): ?string {
         $membershipNumber = null;
         if ((int)$this->year == (int)config('app.year')) {
             switch ($this->type) {
                 case 'family':
-                    $membershipNumber = 'FM-' . str_pad((string)$this->id, '5', '0', STR_PAD_LEFT);
+                    $membershipNumber = 'FM-' . str_pad((string)$this->id, 5, '0', STR_PAD_LEFT);
                     break;
                 case 'individual':
                 default:
-                    $membershipNumber = 'SM-' . str_pad((string)$this->id, '5', '0', STR_PAD_LEFT);
+                    $membershipNumber = 'SM-' . str_pad((string)$this->id, 5, '0', STR_PAD_LEFT);
                     break;
             }
         }
         return $membershipNumber;
+    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo('App\User');
+    }
+    public function entrant(): BelongsTo {
+        return $this->belongsTo('App\Entrant');
     }
 
 }
