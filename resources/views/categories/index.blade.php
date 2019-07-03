7089ee96b9be07c4db6ba609629e2b7491d53464
Toby Powell-Blyth <toby@thegiltclub.com>
2019-07-03 15:03:09 +0100

    Allow viewing the category list with different years\' data

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -11,7 +11,7 @@
     @endphp
     <div class="content">
         <div class="container-fluid">
-            @if($isAdmin)
+            @if($isAdmin && $is_current_year)
             <div class="row">
                 <div class="col-md-12 text-right">
                     <a href="{{ route('category.tabletopprint') }}" target="_blank" class="btn btn-sm btn-primary">{{ __('Print table-top cards') }}</a>
@@ -24,7 +24,7 @@
                     <div class="card">
                         <div class="card-header card-header-success">All categories</div>
                         <div class="card-body">
-                            <p>You can see the categories for this show, along with all winners from the {{config('app.year')}} show, if available, here.</p>
+                            <p>You can see the categories for this show, along with all winners from the {{$year}} show, if available, here.</p>
                         </div>
                     </div>
                 </div>
@@ -40,7 +40,7 @@
                             </div>
 
                             <div class="card-body">
-                                @if (!$publishMode && $isAdmin)
+                                @if (!$publishMode && $isAdmin && $is_current_year)
                                     <p><a class="button btn btn-success"
                                           href="/categories/resultsentry?section={{urlencode($section)}}">Enter
                                             Results</a></p>
