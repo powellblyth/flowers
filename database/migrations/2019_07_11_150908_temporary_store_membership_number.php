137e332ae666b01ec166600e60cf265706abd6ae
Toby Powell-Blyth <toby@thegiltclub.com>
2019-07-11 16:32:53 +0100

    Missed a migration

---
+++ database/migrations/2019_07_11_150908_temporary_store_membership_number.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TemporaryStoreMembershipNumber extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('membership_purchases', function (Blueprint $table) {
+            $table->string('number')->nullable();
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('membership_purchases', function (Blueprint $table) {
+            $table->dropColumn(['number']);
+        });
+    }
+}
