c91cf17233ef46981fc1efe67583b97aeeb6d2e4
Toby Powell-Blyth <toby@powellblyth.com>
2020-05-04 10:07:20 +0100

    fix guest layout

--- resources/views/layouts/page_templates/guest.blade.php
+++ resources/views/layouts/page_templates/guest.blade.php
@@ -1,12 +1,12 @@
 @include('layouts.navbars.navs.guest')
-    <div class="wrapper wrapper-full-page">
-        <div class="page-header login-page header-filter" filter-color="black"
-             style="background-image: url('/images/hero/7712882294_07d8ebd979_o.jpg'); background-size: cover; background-position: top center;align-items: center;"
-             data-color="purple">
-            <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
-            <div class="content">
+<div class="wrapper wrapper-full-page">
+    <div class="page-header login-page header-filter" filter-color="black"
+         style="background-image: url('/images/hero/7712882294_07d8ebd979_o.jpg'); background-size: cover; background-position: top center;align-items: center;"
+         data-color="purple">
+        <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
+        <div class="container" style="height: auto;">
             @yield('content')
-            @include('layouts.footers.guest')
         </div>
+        @include('layouts.footers.guest')
     </div>
 </div>
