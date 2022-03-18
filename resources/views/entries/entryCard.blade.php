f3668e9c4aa141a0b662b1f4b1358c2101fe5cc4
Toby Powell-Blyth <toby@powellblyth.com>
2022-03-18 13:51:26 +0000

    Layout optimisations, laravel upgrade

--- resources/views/entries/entryCard.blade.php
+++ resources/views/entries/entryCard.blade.php
@@ -27,8 +27,11 @@
                 <h3 class=" bg-white text-xl sticky w-full top-10 p-2 font-bold">@lang(':name\'s entry card', ['name'=>$entrant->full_name]) {{$entrant->age_description}}</h3>
                 <div>
                     <div class=" p-2 px-4">
-                        @if($entrant->teams()->wherePivot('show_id', $show->id)->first())
-                            @lang('A member of team :teamname', ['teamname'=>$entrant->teams()->wherePivot('show_id', $show->id)->first()])
+                        @php
+                            $teams = $entrant->teams()->wherePivot('show_id', $show->id)->first();
+                        @endphp
+                        @if($teams)
+                            @lang('A member of team :teamname', ['teamname'=>$teams])
                         @else
                             @if($entrant->isChild())
                                 @lang('Not yet a member of any team')
@@ -37,26 +40,24 @@
                     </div>
                     <div>
                         @foreach($sections as $section)
-                            <div class="px-2 text-xl font-bold bg-pink-200 rounded-md">{{$section->display_name}}</div>
-                            <div class="flex flex-wrap">
+                            <div class="pl-2 text-xl font-bold bg-pink-200 rounded-md">{{$section->display_name}}</div>
+                            <div class="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2">
                             @forelse($show->categories->where('section_id', $section->id)->sortBy('sortorder') as $category)
                                 <!-- TODO dont bother showing unentered historic categories -->
-                                    <div class="flex-initial w-1/4 ">
-                                        <div class="p-2  flex bg-green-200 mx-2 my-3 rounded-xl">
-                                            <label for="{{'cb_' . $category->id . '_' . $entrant->id}}"
-                                                   class="flex-auto w-80 px-2">{{$category->numbered_name}}</label>
-                                            <div class="flex-1 2xl:text-xl">
-                                                <input
-                                                    id="{{'cb_' . $category->id . '_' . $entrant->id}}"
-                                                    @if(!$can_enter)
-                                                        disabled="disabled"
-                                                    @endif
-                                                    @if($entrant->entries->where('category_id', $category->id)->first())
-                                                        checked="checked"
-                                                    @endif
-                                                    type="checkbox" name="entries[{{$entrant->id}}][{{$category->id}}]"
-                                                    class="w-8 h-8 m-2"/>
-                                            </div>
+                                    <div class="p-2 flex bg-green-200 mx-2 my-3 rounded-xl">
+                                        <label for="{{'cb_' . $category->id . '_' . $entrant->id}}"
+                                               class="flex-auto w-80 px-2">{{$category->numbered_name}}</label>
+                                        <div class="2xl:text-xl">
+                                            <input
+                                                id="{{'cb_' . $category->id . '_' . $entrant->id}}"
+                                                @if(!$can_enter)
+                                                disabled="disabled"
+                                                @endif
+                                                @if($entrant->entries->where('category_id', $category->id)->first())
+                                                checked="checked"
+                                                @endif
+                                                type="checkbox" name="entries[{{$entrant->id}}][{{$category->id}}]"
+                                                class="w-6 h-6 m-1"/>
                                         </div>
                                     </div>
                                 @empty
--- resources/views/layouts/page_templates/guest.blade.php
+++ resources/views/layouts/page_templates/guest.blade.php
@@ -1,7 +1,7 @@
 @include('layouts.navbars.navs.guest')
 <div class="wrapper wrapper-full-page">
     <div class="page-header login-page header-filter" filter-color="black"
-         style="background-image: url('/images/hero/7712882294_07d8ebd979_o.jpg'); background-size: cover; background-position: top center;align-items: center;"
+         style="background-image: url('/images/hero/hero-small.png'); background-size: cover; background-position: top center;align-items: center;"
          data-color="purple">
         <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
         <div class="container" style="height: auto;">
