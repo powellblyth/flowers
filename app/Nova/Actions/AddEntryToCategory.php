36ac35c84fe1effcb133b9b6d2e83d3b5c751943
Toby Powell-Blyth <toby@powellblyth.com>
2022-03-21 15:04:34 +0000

    Allow creation of entrant if non exists

--- app/Nova/Actions/AddEntryToCategory.php
+++ app/Nova/Actions/AddEntryToCategory.php
@@ -5,11 +5,13 @@ namespace App\Nova\Actions;
 use App\Models\Category;
 use App\Models\Entrant;
 use App\Models\Entry;
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Collection;
 use Laravel\Nova\Actions\Action;
 use Laravel\Nova\Fields\ActionFields;
+use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Fields\Text;
 
@@ -26,11 +28,34 @@ class AddEntryToCategory extends Action
      */
     public function handle(ActionFields $fields, Collection $models)
     {
-        $models->each(function (Category $category) use ($fields) {
+
+        if ((int) ($fields['entrant_id'] ?? null) > 0) {
+            $entrantId = (int) $fields['entrant_id'];
+        } else {
+            $user = new User();
+            $user->first_name = $fields['first_name'];
+            $user->last_name = $fields['family_name'];
+            $user->email = $fields['email'];
+            $user->address_1 = $fields['address_1'];
+            $user->address_2 = $fields['address_2'];
+            $user->address_town = $fields['address_town'];
+            $user->telephone = $fields['telephone'];
+            $user->postcode = $fields['postcode'];
+            $user->can_email = $fields['can_email'];
+            $user->can_phone = $fields['can_phone'];
+            $user->can_retain_data = $fields['can_retain_data'];
+            $user->save();
+
+            $entrantId = $user->refresh()->entrants->first()->id;
+        }
+//        $entry->entrant_id = ;
+
+
+        $models->each(function (Category $category) use ($fields, $entrantId) {
             $entry = new Entry();
             $entry->category()->associate($category);
-            $entry->entrant_id = $fields['entrant_id'];
             $entry->show()->associate($category->show);
+            $entry->entrant_id = $entrantId;
             $entry->save();
         });
     }
@@ -43,13 +68,24 @@ class AddEntryToCategory extends Action
     public function fields()
     {
         return [
-            Text::make('nothing'),
-            Select::make('entrant', 'entrant_id')
+            Select::make(__('Entrant'), 'entrant_id')
                 ->searchable()
-                ->rules('required', 'exists:entrants,id')
+                ->rules('nullable', 'exists:entrants,id')
                 ->options(Entrant::where('is_anonymised', false)->orderBy('family_name')->orderBy('first_name')
                     ->get()
-                    ->mapWithKeys(fn(Entrant $entrant) => [$entrant->id=>$entrant->full_name])),
+                    ->mapWithKeys(fn(Entrant $entrant) => [$entrant->id => $entrant->full_name])),
+            Text::make(__('First Name'), 'first_name'),
+            Text::make(__('Family Name'), 'family_name'),
+            Text::make(__('Email Address'), 'email'),
+            Text::make(__('Phone Number'), 'telephone'),
+            Text::make('Address 1')->hideFromIndex(),
+            Text::make('Address 2')->hideFromIndex(),
+            Text::make('Town', 'address_town')->hideFromIndex(),
+            Text::make('Postcode')->hideFromIndex(),
+            Text::make('Telephone')->hideFromIndex(),
+            Boolean::make(__('Can Retain Data'), 'can_retain_data'),
+            Boolean::make(__('Can Email'), 'can_email'),
+            Boolean::make(__('Can Telephone'), 'can_telephone'),
         ];
     }
 }
