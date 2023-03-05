52c6ed074408ee6066714080e1a3a11524c54267
Toby Powell-Blyth <toby@powellblyth.com>
2023-03-05 19:02:19 +0000

    More stuff

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    plugins: {
+        'postcss-import': {},
+        tailwindcss: {
+            content: [
+                "./resources/**/*.blade.php",
+                "./resources/**/*.js",
+                "./resources/**/*.vue",
+                './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+                './storage/framework/views/*.php',
+            ],
+
+            theme: {
+                extend: {
+                    fontFamily: {
+                        sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                    },
+                },
+            },
+
+            plugins: [
+                require('@tailwindcss/forms')
+            ],
+        },
+        autoprefixer: {},
+    },
 }
