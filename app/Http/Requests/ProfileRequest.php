1b445138c144e460151f4c3609695fdf6c80920f
Toby Powell-Blyth <toby@powellblyth.com>
2023-03-05 16:14:27 +0000

    Profile update fix

--- app/Http/Requests/ProfileRequest.php
+++ app/Http/Requests/ProfileRequest.php
@@ -13,7 +13,7 @@ class ProfileRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return auth()->check();
     }
@@ -23,14 +23,14 @@ class ProfileRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'first_name' => ['required', 'min:3'],
             'last_name' => ['required', 'min:3'],
             'address_1' => ['nullable', 'min:3'],
             'address_2' => ['nullable', 'min:3'],
-            'telephone' => ['nullable', 'regex:/(0)[0-9]{10}', 'min:ll'],
+            'telephone' => ['nullable', 'regex:/\+?[\(\)0-9 ]*/', 'min:11'],
             'address_town' => ['required_with:address_1', 'min:3'],
             'postcode' => ['required_with:address_1', 'min:3'],
             'email' => ['required', 'email', Rule::unique((new User)->getTable())->ignore(auth()->id())],
