1156f0b3d7175e37727f638d525b69a4771613c3
Toby Powell-Blyth <toby@powellblyth.com>
2023-05-18 18:30:49 +0100

    spell judge properly

--- resources/views/cups/show.blade.php
+++ resources/views/cups/show.blade.php
@@ -27,49 +27,12 @@
             @if ($cup->is_points_based)
                 @lang('For the most points in ')
             @else
-                @lang('The Juge\'s choice from ')
+                @lang('The Judge\'s choice from ')
             @endif
             {{(count($categories) > 1 ? 'these categories': 'this category') }}</b><br/>
 
         @foreach ($categories as $category)
             <p>{{$category->numbered_name}} <small>{{$category->notes}}</small></p>
-            {{--                        @if (array_key_exists($category->id, $winners_by_category) && count($winners_by_category[$category->id]) > 0)--}}
-            {{--                            <td>--}}
-            {{--                                @if (array_key_exists('1', $winners_by_category[$category->id]))--}}
-            {{--                                    {{$winners[$winners_by_category[$category->id]['1']['entrant']]->printable_name}}--}}
-            {{--                                    ({{$winners_by_category[$category->id]['1']['points']}} points)--}}
-            {{--                                @else--}}
-            {{--                                    ---}}
-            {{--                                @endif--}}
-            {{--                            </td>--}}
-            {{--                            <td>--}}
-            {{--                                @if (array_key_exists('2', $winners_by_category[$category->id]))--}}
-            {{--                                    {{$winners[$winners_by_category[$category->id]['2']['entrant']]->printable_name}}--}}
-            {{--                                    ({{$winners_by_category[$category->id]['2']['points']}} points)--}}
-            {{--                                @else--}}
-            {{--                                    ---}}
-            {{--                                @endif--}}
-            {{--                            </td>--}}
-            {{--                            <td>--}}
-            {{--                                @if (array_key_exists('3', $winners_by_category[$category->id]))--}}
-            {{--                                    {{$winners[$winners_by_category[$category->id]['3']['entrant']]->printable_name}}--}}
-            {{--                                    ({{$winners_by_category[$category->id]['3']['points']}} points)--}}
-            {{--                                @else--}}
-            {{--                                    ---}}
-            {{--                                @endif--}}
-            {{--                            </td>--}}
-            {{--                            <td>--}}
-            {{--                                @if (array_key_exists('commended', $winners_by_category[$category->id]))--}}
-            {{--                                    {{$winners[$winners_by_category[$category->id]['commended']['entrant']]->printable_name}}--}}
-            {{--                                    ({{$winners_by_category[$category->id]['commended']['points']}} points)--}}
-            {{--                                @else--}}
-            {{--                                    ---}}
-            {{--                                @endif--}}
-            {{--                            </td>--}}
-            {{--                        @else--}}
-            {{--                            <td colspan="4">@lang('Unavailable')</td>--}}
-            {{--                        @endif--}}
-
         @endforeach
 
 
