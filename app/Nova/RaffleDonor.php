ba64a6988fc2d186bc531d178199fc36d0291134
Toby Powell-Blyth <toby@powellblyth.com>
2023-05-30 21:13:24 +0100

    Better name for raffle donor

--- app/Nova/RaffleDonor.php
+++ app/Nova/RaffleDonor.php
@@ -27,7 +27,7 @@ class RaffleDonor extends Resource
      *
      * @var string
      */
-    public static $title = 'id';
+    public static $title = 'name';
 
     /**
      * The columns that should be searched.
