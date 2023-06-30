8a89e2c8449b260bc1e6d8d4c7a0bfe2d758d820
Toby Powell-Blyth <toby@powellblyth.com>
2023-06-30 22:47:47 +0100

    Schedule age update command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\CheckExpiredMemberships;
+use App\Console\Commands\UpdateAgesCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -35,6 +36,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
 //         $schedule->command('inspire')->hourly();
+        $schedule->command(UpdateAgesCommand::class)->yearlyOn(1, 1);
         $schedule->command(CheckExpiredMemberships::class)->hourlyAt(12);
     }
 
