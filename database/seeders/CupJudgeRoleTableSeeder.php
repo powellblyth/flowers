bb9dfb7a4bfd218d6ed75c5cfffdb63a18e87aa5
Toby Powell-Blyth <toby@powellblyth.com>
2024-06-09 12:42:24 +0100

    Better database seeders

--- database/seeders/CupJudgeRoleTableSeeder.php
+++ database/seeders/CupJudgeRoleTableSeeder.php
@@ -103,7 +103,7 @@ class CupJudgeRoleTableSeeder extends Seeder
             array (
                 'id' => 11,
                 'cup_id' => 16,
-                'judge_role_id' => 8,
+                'judge_role_id' => 9,
                 'created_at' => NULL,
                 'updated_at' => NULL,
             ),
@@ -111,7 +111,31 @@ class CupJudgeRoleTableSeeder extends Seeder
             array (
                 'id' => 12,
                 'cup_id' => 15,
-                'judge_role_id' => 8,
+                'judge_role_id' => 9,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            12 => 
+            array (
+                'id' => 13,
+                'cup_id' => 29,
+                'judge_role_id' => 1,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            13 => 
+            array (
+                'id' => 14,
+                'cup_id' => 29,
+                'judge_role_id' => 2,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            14 => 
+            array (
+                'id' => 15,
+                'cup_id' => 29,
+                'judge_role_id' => 3,
                 'created_at' => NULL,
                 'updated_at' => NULL,
             ),
