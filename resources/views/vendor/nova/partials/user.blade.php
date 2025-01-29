cab92c5a6e12ddf0c48778f7e37de557ff2241bf
Toby Powell-Blyth <toby@powellblyth.com>
2025-01-29 18:18:54 +0000

    SOme ui language tweaks including admin links to front end, added hot refresh

--- resources/views/vendor/nova/partials/user.blade.php
+++ resources/views/vendor/nova/partials/user.blade.php
@@ -14,8 +14,23 @@
 <dropdown-menu slot="menu" width="200" direction="rtl">
     <ul class="list-reset">
         <li>
+            <a href="{{ route('family') }}" class="block no-underline text-90 hover:bg-30 p-3">
+                {{ __('Your Family Page') }}
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('categories.index') }}" class="block no-underline text-90 hover:bg-30 p-3">
+                {{ __('Categories') }}
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('cups.index') }}" class="block no-underline text-90 hover:bg-30 p-3">
+                {{ __('Cups') }}
+            </a>
+        </li>
+        <li>
             <a href="{{ route('nova.logout') }}" class="block no-underline text-90 hover:bg-30 p-3">
-                {{ __('Logout') }}
+                {{ __('Log out') }}
             </a>
         </li>
     </ul>
